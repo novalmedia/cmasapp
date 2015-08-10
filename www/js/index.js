37d0c34b75011d95c04f609f661ab2411be31a5a
novalmedia <a.troitino@novalmedia.com>
2015-08-10 17:12:05 +0200

    mixfix

--- www/js/index.js
+++ www/js/index.js
@@ -1,76 +1,76 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        console.log('Received Event: ' + id);
-		var pushNotification = window.plugins.pushNotification;
-		pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"349344466742","ecb":"app.onNotificationGCM"});
-    },
-	errorHandler:function(error) {
-		alert(error);
-	},
-	successHandler: function(result) {
-		alert('Callback Success! Result = '+result)
-	},
-	onNotificationGCM: function(e) {
-        switch( e.event )
-        {
-            case 'registered':
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-            break;
- 
-            case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
- 
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
- 
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
-    }
-};
+function init() {
+	document.addEventListener("deviceready", deviceReady, true);
+	delete init;
+}
 
+function checkPreAuth() {
+	var form = $("#loginForm");
+	if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
+		$("#username", form).val(window.localStorage["username"]);
+		$("#password", form).val(window.localStorage["password"]);
+		handleLogin();
+	}
+}
+
+function handleLogin() {
+	var form = $("#loginForm");
+	//disable the button so we can't resubmit while we wait
+	$("#submitButton",form).attr("disabled","disabled");
+	var u = $("#username", form).val();
+	var p = $("#password", form).val();
+	console.log("click");
+	if(u != '' && p!= '') {
+		$.post("http://www.clubmascodin.com/app/auth.php", {username:u,password:p}, function(res) {
+			if(res == true) {
+				//store
+				window.localStorage["username"] = u;
+				window.localStorage["password"] = p;
+				var pushNotification = window.plugins.pushNotification;
+				pushNotification.register(successHandler, errorHandler,{"senderID":"349344466742","ecb":"onNotificationGCM"});
+
+			} else {
+				navigator.notification.alert("Error de identificación", function() {});
+			}
+			$("#submitButton").removeAttr("disabled");
+		},"json");
+	} else {
+	//Thanks Igor!
+		navigator.notification.alert("Debes introducir tu numero de socio y contraseña", function() {});
+		$("#submitButton").removeAttr("disabled");
+	}
+	return false;
+}
+
+function deviceReady() {
+	$("#loginForm").on("submit",handleLogin);
+}
+
+
+
+function errorHandler(error) {
+	alert(error);
+}
+function successHandler(result) {
+	alert('Callback Success! Result = '+result)
+}
+function onNotificationGCM(e) {
+	switch( e.event )
+	{
+		case 'registered':
+			if ( e.regid.length > 0 )
+			{
+				console.log("Regid " + e.regid);
+				alert('registration id = '+e.regid);
+			}
+			break;
+		case 'message':
+			alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+			break;
+		case 'error':
+			alert('GCM error = '+e.msg);
+			break;
+		default:
+			alert('An unknown GCM event has occurred');
+			break;
+	}
+}
