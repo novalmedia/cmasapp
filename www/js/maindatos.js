f53258ca3da6e55e86fc34dcf81a25835ed3785f
novalmedia <a.troitino@novalmedia.com>
2015-09-03 17:14:27 +0200

    asdasd

--- www/js/maindatos.js
+++ www/js/maindatos.js
@@ -106,7 +106,7 @@ var app = {
 		$('body').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover').css('background-position','center center');
 		$('.ui-page, .ui-content').css('background', 'transparent');
 		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.7)');
-		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:bg}, function(res) {
+		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:imageData}, function(res) {
 			if (res==true){
 				navigator.notification.alert("Fondo guardado", function() {});
 				window.localStorage['bgpictureData'] = imageData;
@@ -119,7 +119,7 @@ var app = {
 		$('body').css('background-image','url(' + imageData + ')').css('background-size','cover').css('background-position','center center');
 		$('.ui-page, .ui-content').css('background', 'transparent');
 		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.7)');
-		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:bg}, function(res) {
+		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:imageData}, function(res) {
 			if (res==true){
 				navigator.notification.alert("Fondo guardado", function() {});
 				window.localStorage['bgpictureData'] = '';
