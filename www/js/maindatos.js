f3371962678ac32f792fde853e25cc7a1901a09c
novalmedia <a.troitino@novalmedia.com>
2015-09-03 16:38:27 +0200

    asdasdad

--- www/js/maindatos.js
+++ www/js/maindatos.js
@@ -104,14 +104,14 @@ var app = {
 	},
 	onPhotoDataSuccess: function(imageData) {
 		$('body').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover');
-		$('.ui-panel-wrapper').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover');
-		$('.ui-page, .ui-content, .ui-btn').css('background', 'transparent');
+		//$('.ui-panel-wrapper').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover');
+		$('.ui-page, .ui-content, .ui-panel-wrapper').css('background', 'transparent');
 		//$('#picpreview').html(imageData);
 	},
 	onPhotoFileSuccess: function(imageData) { 
 		$('body').css('background-image','url(' + imageData + ')').css('background-size','cover');
-		$('.ui-panel-wrapper').css('background-image','url(' + imageData + ')').css('background-size','cover');
-		$('.ui-page, .ui-content, .ui-btn').css('background', 'transparent');
+//		$('.ui-panel-wrapper').css('background-image','url(' + imageData + ')').css('background-size','cover');
+		$('.ui-page, .ui-content, .ui-panel-wrapper').css('background', 'transparent');
 		//$('#picpreview').html(imageData);
 	}
 	
