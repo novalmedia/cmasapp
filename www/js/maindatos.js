8fffe849407e2161afdae68ab80d0619b967bd68
novalmedia <a.troitino@novalmedia.com>
2015-09-03 14:29:14 +0200

    asdasd

--- www/js/maindatos.js
+++ www/js/maindatos.js
@@ -34,8 +34,6 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		pictureSource=navigator.camera.PictureSourceType;
-		destinationType=navigator.camera.DestinationType;
 		
     },
     // Update DOM on a Received Event
