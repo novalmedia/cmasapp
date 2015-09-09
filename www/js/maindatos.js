790745091e90b2e367a683c45881d005ecbf689a
novalmedia <a.troitino@novalmedia.com>
2015-09-09 19:04:19 +0200

    asdasdad

--- www/js/maindatos.js
+++ www/js/maindatos.js
@@ -110,29 +110,31 @@ var app = {
 		console.log(fail);
 	},
 	onPhotoDataSuccess: function(imageData) {
-		$('body').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover').css('background-position','center center');
+		/* $('body').css('background-image','url(data:image/jpeg;base64,' + imageData + ')').css('background-size','cover').css('background-position','center center');
 		$('.ui-page, .ui-content').css('background', 'transparent');
-		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.5)');
+		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.5)'); */
 		var uid = window.localStorage["userid"];
 		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:imageData}, function(res) {
 			if (res==true){
 				navigator.notification.alert("Fondo guardado", function() {});
 				window.localStorage['bgpictureData'] = imageData;
 				window.localStorage['bgpictureFile'] = '';
+				window.location = "datos.html";
 			}
 		},"json");
 		
 	},
 	onPhotoFileSuccess: function(imageData) { 
-		$('body').css('background-image','url(' + imageData + ')').css('background-size','cover').css('background-position','center center');
+		/* $('body').css('background-image','url(' + imageData + ')').css('background-size','cover').css('background-position','center center');
 		$('.ui-page, .ui-content').css('background', 'transparent');
-		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.5)');
+		$('.ui-panel-wrapper').css('background', 'rgba(255,255,255,0.5)'); */
 		var uid = window.localStorage["userid"];
 		$.post("http://www.clubmascodin.com/app/savebg.php", {userid:uid,bg:imageData}, function(res) {
 			if (res==true){
 				navigator.notification.alert("Fondo guardado", function() {});
 				window.localStorage['bgpictureData'] = '';
 				window.localStorage['bgpictureFile'] = imageData;
+				window.location = "datos.html";
 			}
 		},"json");
 
